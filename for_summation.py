55a5817ee303a19fbc037238473f0bd1f5aeb860
Lorgan <brownlog002@alpinesd.org>
2025-09-22 10:55:41 -0600

    Update for_summation.py

--- for_summation.py
+++ for_summation.py
@@ -1,1 +1,7 @@
-#for Summation code here
+summation = int(input("What number do you want a summation for: "))
+answer=0
+solution=0
+x=summation
+for x in range(summation+1):
+    answer=x+answer
+print(answer)
