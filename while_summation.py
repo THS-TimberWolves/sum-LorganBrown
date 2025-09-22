9b466bf195a2ae6376557f3ae485309acf6b6f32
Lorgan <brownlog002@alpinesd.org>
2025-09-22 10:56:10 -0600

    Update while_summation.py

--- while_summation.py
+++ while_summation.py
@@ -1,1 +1,8 @@
-#while summation code following directions from Readme
+summation = int(input("What number do you want a summation for: "))
+answer=0
+solution=0
+x=summation
+while x >= 0:
+    solution=x+solution
+    x=x-1
+print(solution)
